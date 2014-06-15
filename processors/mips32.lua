30245f76beee44e14a55f8131403b7bc26a52f7f
Dax <trogu.davide@gmail.com>
2014-06-15 13:32:28 +0200

    MIPS Processor: Added Missing Registers, minor fixes

--- processors/mips32.lua
+++ processors/mips32.lua
@@ -63,7 +63,7 @@ local MIPS32InstructionSet = { [0x00000020] = { mnemonic = "ADD", category = Ins
                                [0x70000004] = { mnemonic = "MSUB", category = InstructionCategory.Arithmetic, type = InstructionType.Undefined },
                                [0x70000005] = { mnemonic = "MSUBU", category = InstructionCategory.Arithmetic, type = InstructionType.Undefined },
                                [0x00000011] = { mnemonic = "MTHI", category = InstructionCategory.LoadStore, type = InstructionType.Undefined },
-                               [0x00000013] = { mnemonic = "MTLO", category = InstructionCategory.Arithmetic, type = InstructionType.Undefined },
+                               [0x00000013] = { mnemonic = "MTLO", category = InstructionCategory.LoadStore, type = InstructionType.Undefined },
                                [0x70000002] = { mnemonic = "MUL", category = InstructionCategory.Arithmetic, type = InstructionType.Mul },
                                [0x00000018] = { mnemonic = "MULT", category = InstructionCategory.Arithmetic, type = InstructionType.Mul },
                                [0x00000019] = { mnemonic = "MULTU", category = InstructionCategory.Arithmetic, type = InstructionType.Mul },
@@ -98,36 +98,40 @@ local MIPS32InstructionSet = { [0x00000020] = { mnemonic = "ADD", category = Ins
                                [0x00000026] = { mnemonic = "XOR", category = InstructionCategory.Logical, type = InstructionType.Xor },
                                [0x38000000] = { mnemonic = "XORI", category = InstructionCategory.Logical, type = InstructionType.Xor } }
 
-local MIPS32OpCodes = { Math_ADD    = 0x00000020, Math_ADDI    = 0x20000000, Math_ADDIU = 0x24000000, Math_ADDU   = 0x00000021, Log_AND     = 0x00000024, Log_ANDI   = 0x30000000, 
-                        Bra_BEQ     = 0x10000000, Bra_BEQL     = 0x50000000, Bra_BGEZ   = 0x04010000, Bra_BGEZA   = 0x04110000, Bra_BGEZALL = 0x04130000, Bra_BGEZL  = 0x04030000,
-                        Bra_BGTZ    = 0x1C000000, Bra_BGTZL    = 0x5C000000, Bra_BLEZ   = 0x18000000, Bra_BLEZL   = 0x58000000, Bra_BLTZ    = 0x04000000, Bra_BLTZAL = 0x04100000,
-                        Bra_BLTZALL = 0x04120000, Bra_BLTZL    = 0x04020000, Bra_BNE    = 0x14000000, Bra_BNEL    = 0x54000000, Trap_BREAK  = 0x0000000D, Priv_CACHE = 0xBC000000,
-                        Cop_CFC2    = 0x48400000, Math_CLO     = 0x70000021, Math_CLZ   = 0x70000020, Cop_COP2    = 0x4A000000, Cop_CTC2    = 0x48C00000, Math_DIV   = 0x0000001A,
-                        Math_DIVU   = 0x0000001B, Ctrl_EHB     = 0x000000C0, Branch_J   = 0x08000000, Branch_JAL  = 0x0C000000, Branch_JALR = 0x00000009, Branch_JR  = 0x00000008,
-                        Mem_LB      = 0x80000000, Mem_LBU      = 0x90000000, Mem_LH     = 0x84000000, Mem_LHU     = 0x94000000, Mem_LL      = 0xC0000000, Log_LUI    = 0x3C000000,
-                        Mem_LW      = 0x8C000000, Cop_LWC2     = 0xC8000000, Mem_LWL    = 0x88000000, Mem_LWR     = 0x98000000, Math_MADD   = 0x70000000, Math_MADDU = 0x70000001,
-                        Move_MFHI   = 0x00000010, Move_MFLO    = 0x00000012, Move_MOVN  = 0x0000000B, Move_MOVZ   = 0x0000000A, Math_MSUB   = 0x70000004, Math_MSUBU = 0x70000005,
-                        Move_MTHI   = 0x00000011, Move_MTLO    = 0x00000013, Math_MUL   = 0x70000002, Math_MULT   = 0x00000018, Math_MULTU  = 0x00000019, Log_NOR    = 0x00000027,
-                        Log_OR      = 0x00000025, Log_ORI      = 0x34000000, Priv_RDHWR = 0x7C00003B, Priv_RDPGPR = 0x41400000, Mem_SB      = 0xA0000000, Mem_SC     = 0xE0000000,
-                        Math_SEB    = 0x7C000420, Math_SEH     = 0x7C000620, Mem_SH     = 0xA4000000, Log_SLL     = 0x00000000, Log_SLLV    = 0x00000004, Math_SLT   = 0x0000002A,
-                        Math_SLTI   = 0x28000000, Math_SLTIU   = 0x2C000000, Math_SLTU  = 0x0000002B, Log_SRA     = 0x00000003, Log_SRAV    = 0x00000007, Log_SRL    = 0x00000002,
-                        Log_SRLV    = 0x00000006, Math_SUB     = 0x00000022, Math_SUBU  = 0x00000023, Mem_SW      = 0xAC000000, Cop_SWC2    = 0xE8000000, Mem_SWL    = 0xA8000000,
-                        Mem_SWR     = 0xB8000000, Trap_SYSCALL = 0x0000000C, Trap_TGE   = 0x00000030, Log_XOR     = 0x00000026, Log_XORI    = 0x38000000 }
+local MIPS32OpCodes = { Math_ADD       = 0x00000020, Math_ADDI      = 0x20000000, Math_ADDIU     = 0x24000000, Math_ADDU      = 0x00000021, Log_AND        = 0x00000024, 
+                        Log_ANDI       = 0x30000000, Branch_BEQ     = 0x10000000, Branch_BEQL    = 0x50000000, Branch_BGEZ    = 0x04010000, Branch_BGEZAL  = 0x04110000, 
+                        Branch_BGEZALL = 0x04130000, Branch_BGEZL   = 0x04030000, Branch_BGTZ    = 0x1C000000, Branch_BGTZL   = 0x5C000000, Branch_BLEZ    = 0x18000000, 
+                        Branch_BLEZL   = 0x58000000, Branch_BLTZ    = 0x04000000, Branch_BLTZAL  = 0x04100000, Branch_BLTZALL = 0x04120000, Branch_BLTZL   = 0x04020000, 
+                        Branch_BNE     = 0x14000000, Branch_BNEL    = 0x54000000, Trap_BREAK     = 0x0000000D, Priv_CACHE     = 0xBC000000, Cop_CFC2       = 0x48400000, 
+                        Math_CLO       = 0x70000021, Math_CLZ       = 0x70000020, Cop_COP2       = 0x4A000000, Cop_CTC2       = 0x48000000, Math_DIV       = 0x0000001A, 
+                        Math_DIVU      = 0x0000001B, Ctrl_EHB       = 0x000000C0, Branch_J       = 0x08000000, Branch_JAL     = 0x0C000000, Branch_JALR    = 0x00000009, 
+                        Branch_JR      = 0x00000008, Mem_LB         = 0x80000000, Mem_LBU        = 0x90000000, Mem_LH         = 0x84000000, Mem_LHU        = 0x94000000, 
+                        Mem_LL         = 0xC0000000, Mem_LUI        = 0x3C000000, Mem_LW         = 0x8C000000, Cop_LWC2       = 0xC8000000, Mem_LWL        = 0x88000000, 
+                        Mem_LWR        = 0x98000000, Math_MADD      = 0x70000000, Math_MADDU     = 0x70000001, Move_MFHI      = 0x00000010, Move_MFLO      = 0x00000012, 
+                        Move_MOVN      = 0x0000000B, Move_MOVZ      = 0x0000000A, Math_MSUB      = 0x70000004, Math_MSUBU     = 0x70000005, Move_MTHI      = 0x00000011, 
+                        Move_MTLO      = 0x00000013, Math_MUL       = 0x70000002, Math_MULT      = 0x00000018, Math_MULTU     = 0x00000019, Log_NOR        = 0x00000027, 
+                        Log_OR         = 0x00000025, Log_ORI        = 0x34000000, Priv_RDHWR     = 0x7C00003B, Priv_RDPGPR    = 0x41400000, Mem_SB         = 0xA0000000, 
+                        Mem_SC         = 0xE0000000, Math_SEB       = 0x7C000420, Math_SEH       = 0x7C000620, Mem_SH         = 0xA4000000, Log_SLL        = 0x00000000, 
+                        Log_SLLV       = 0x00000004, Math_SLT       = 0x0000002A, Math_SLTI      = 0x28000000, Math_SLTIU     = 0x2C000000, Math_SLTU      = 0x0000002B, 
+                        Math_SRA       = 0x00000003, Math_SRAV      = 0x00000007, Log_SRL        = 0x00000002, Log_SRLV       = 0x00000006, Math_SUB       = 0x00000022, 
+                        Math_SUBU      = 0x00000023, Mem_SW         = 0xAC000000, Cop_SWC2       = 0xE8000000, Mem_SWL        = 0xA8000000, Mem_SWR        = 0xB8000000, 
+                        Trap_SYSCALL   = 0x0000000C, Trap_TGE       = 0x00000030, Log_XOR        = 0x00000026, Log_XORI       = 0x38000000 }
 
-local MIPS32RegisterNames = { [0]  = "$zero", [1]  = "$at", [2]  = "$v0", [3]  = "$v1", [4]  = "$a0", 
-                              [5]  = "$a1",   [6]  = "$v2", [7]  = "$a3", [8]  = "$t0", [9]  = "$t1",
-                              [10] = "$t2",   [11] = "$t3", [12] = "$t4", [13] = "$t5", [14] = "$t6",
-                              [15] = "$t7",   [16] = "$s0", [17] = "$s1", [18] = "$s2", [19] = "$s3",
-                              [20] = "$s4",   [21] = "$s5", [22] = "$s6", [23] = "$s7", [25] = "$r9",
-                              [26] = "$k0",   [27] = "$k1", [28] = "$gp", [29] = "$sp", [30] = "$fp",
-                              [31] = "$ra" }
+local MIPS32RegisterNames = { [0]  = "zero", [1]  = "at", [2]  = "v0", [3]  = "v1", [4] = "a0", 
+                              [5]  = "a1",   [6]  = "a2", [7]  = "a3", [8]  = "t0", [9] = "t1", 
+                              [10] = "t2",   [11] = "t3", [12] = "t4", [13] = "t5", [14] = "t6", 
+                              [15] = "t7",   [16] = "s0", [17] = "s1", [18] = "s2", [19] = "s3", 
+                              [20] = "s4",   [21] = "s5", [22] = "s6", [23] = "s7", [24] = "t8", 
+                              [25] = "t9",   [26] = "k0", [27] = "k1", [28] = "gp", [29] = "sp", 
+                              [30] = "fp",   [31] = "ra" }
 
-local MIPS32Registers = { Reg_ZERO = 0,  Reg_AT = 1,  Reg_V0 = 2,  Reg_V1 = 3,  Reg_A0 = 4, 
-                          Reg_A1   = 5,  Reg_V2 = 6,  Reg_A3 = 7,  Reg_T0 = 8,  Reg_T1 = 9, 
-                          Reg_T2   = 10, Reg_T3 = 11, Reg_T4 = 12, Reg_T5 = 13, Reg_T6 = 14,
-                          Reg_T7   = 15, Reg_S1 = 17, Reg_S2 = 18, Reg_S3 = 19, Reg_S4 = 20,
-                          Reg_S5   = 21, Reg_S6 = 22, Reg_S7 = 23, Reg_R9 = 25, Reg_K0 = 26,
-                          Reg_K1   = 27, Reg_GP = 28, Reg_SP = 29, Reg_FP = 30, Reg_RA = 31 }
+local MIPS32Registers = { Reg_ZERO = 0,  Reg_AT   = 1,  Reg_V0   = 2,  Reg_V1   = 3,  Reg_A0   = 4, 
+                          Reg_A1   = 5,  Reg_A2   = 6,  Reg_A3   = 7,  Reg_T0   = 8,  Reg_T1   = 9, 
+                          Reg_T2   = 10, Reg_T3   = 11, Reg_T4   = 12, Reg_T5   = 13, Reg_T6   = 14, 
+                          Reg_T7   = 15, Reg_S0   = 16, Reg_S1   = 17, Reg_S2   = 18, Reg_S3   = 19, 
+                          Reg_S4   = 20, Reg_S5   = 21, Reg_S6   = 22, Reg_S7   = 23, Reg_T8   = 24, 
+                          Reg_T9   = 25, Reg_K0   = 26, Reg_K1   = 27, Reg_GP   = 28, Reg_SP   = 29, 
+                          Reg_FP   = 30, Reg_RA   = 31 }
 
 local MIPS32Processor = oop.class(ProcessorDefinition)
 
@@ -236,10 +240,10 @@ function MIPS32Processor:analyze(instruction)
   end
   
   if (instruction.opcode == self.opcodes.Branch_J) or (instruction.opcode == self.opcodes.Branch_JAL) then
-    instruction:addOperand(OperandType.Address, bit.lshift(bit.band(data, 0x3FFFFFF), 2))                               -- instr_index
+    instruction:addOperand(OperandType.Address, bit.lshift(bit.band(data, 0x3FFFFFF), 2))                      -- instr_index
   else
-    instruction:addOperand(OperandType.Register, bit.rshift(bit.band(data, 0x03E00000), 0x15))           -- rs
-    instruction:addOperand(OperandType.Register, bit.rshift(bit.band(data, 0x001F0000), 0x10))           -- rt
+    instruction:addOperand(OperandType.Register, bit.rshift(bit.band(data, 0x03E00000), 0x15))                 -- rs
+    instruction:addOperand(OperandType.Register, bit.rshift(bit.band(data, 0x001F0000), 0x10))                 -- rt
     instruction:addOperand(OperandType.Immediate, tonumber(ffi.cast("int32_t", bit.band(data, 0x0000FFFF))))   -- immediate
   end
   
@@ -257,11 +261,9 @@ function MIPS32Processor:emulate(listing, instruction)
     listing:push(instruction.operands[1].value, ReferenceType.Call)
   elseif instruction.opcode == self.opcodes.Branch_J then
     listing:push(instruction.operands[1].value, ReferenceType.Jump)
+  elseif (bit.band(instructionset[instruction.opcode].type, InstructionType.Call) ~= 0) or (bit.band(instructionset[instruction.opcode].type, InstructionType.ConditionalCall) ~= 0) then
+    
   end
-  
---   if bit.band(instructionset[instruction.opcode].type, InstructionType.Call) ~= 0) or bit.band(instructionset[instruction.opcode].type, InstructionType.ConditionalCall) ~= 0) then
---     local address = instruction.operands[3]
---   end
     
   listing:push(instruction.address + DataType.sizeOf(DataType.UInt32), ReferenceType.Flow)
 end
