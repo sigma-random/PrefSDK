e38c8b785bb593e23fd0fea041612b3105165eea
Dax <trogu.davide@gmail.com>
2014-08-20 11:21:02 +0200

    Imported Entropy API through FFI

--- sdk/math/functions.lua
+++ sdk/math/functions.lua
@@ -1,7 +1,19 @@
+local ffi = require("ffi")
+
+ffi.cdef
+[[
+  double Math_entropy(void* hexeditdata, uint64_t start, uint64_t size);
+]]
+
+local C = ffi.C
 local MathFunctions = { }
 
 function MathFunctions.logb(n, b)
   return math.log(n) / math.log(b)
+end
+
+function MathFunctions.entropy(databuffer, start, size)
+  return C.Math_entropy(databuffer.cthis, start, size)
 end
 
 return MathFunctions
