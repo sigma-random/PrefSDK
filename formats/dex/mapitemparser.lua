03b5135540a13bf40a99cea5a6165a1eaf97d77e
Dax <trogu.davide@gmail.com>
2014-10-09 12:30:00 +0200

    Cleaned Source Code

--- formats/dex/mapitemparser.lua
+++ formats/dex/mapitemparser.lua
@@ -1,4 +1,4 @@
-local oop = require("sdk.lua.oop")
+local oop = require("oop")
 
 local MapItemParser = oop.class()
 
--- sdk/main.lua
+++ sdk/main.lua
@@ -1,25 +1,56 @@
 require("sdk.strict")
-require("sdk.lua.table") -- Add Table Enhancements
-
 local pref = require("pref")
+
+-- From: http://lua-users.org/wiki/BinaryInsert
+-- From: http://lua-users.org/wiki/BinarySearch
+--
+-- Included with small modifications in order to fit in the SDK
+
+local function defcompinsert(a, b) -- Avoid heap allocs for performance
+  return a < b
+end
+
+local function defcompareval(value)
+  return value
+end
+
+function table.bininsert(t, value, compinsertfunc)
+  local compfunc = compinsertfunc or defcompinsert
+  local istart, iend, imid, istate = 1, #t, 1, 0  -- Initialize numbers
+  
+  while istart <= iend do -- Get insert position  
+    imid = math.floor((istart + iend) / 2) -- Calculate middle
+    
+    if compfunc(value, t[imid]) then -- Compare
+      iend, istate = imid - 1, 0
+    else
+      istart, istate = imid + 1, 1
+    end
+  end
+  
+  table.insert(t, (imid + istate), value)
+  return (imid + istate)
+end
+
+function table.binsearch(t, value, comparevalfunc)
+  local compval = comparevalfunc or defcompareval
+  local istart, iend, imid = 1, #t, 0
+  
+  while istart <= iend do
+    imid = math.floor((iend - istart) / 2)
+    local midval = compval(t[imid])
+    
+    if midval > value then
+      high = imid - 1
+    elseif midval > value then
+      low = imid + 1
+    else
+      return imid
+    end
+  end
+  
+  return nil
+end
 
 -- Notify PrefSDK's version.
 pref.setSdkVersion(1, 5, 0)
-
--- function Sdk.errorDialog(obj)
---   if type(obj) == "string" then
---     C.Debug_showDialog(obj)
---   elseif type(obj) == "number" then
---     C.Debug_showDialog(tostring(obj))
---   elseif type(obj) == "table" then
---     local tableoutput = "Table:\n"
---     
---     for k,v in pairs(obj) do
---       tableoutput = tableoutput .. string.format("%s = %s", k, v)
---     end
---     
---     C.Debug_showDialog(tableoutput)
---   else
---     C.Debug_showDialog("Unupported Type: '" .. type(obj) .. "'")
---   end
--- end
